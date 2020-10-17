6db10ad7e362a3492068f3e845986fa9a44a57df
jordi lopez galera <jordilg@MacBook-Pro-de-jordi-lopez-galera.local>
2020-10-17 11:07:54 +0200

    implement back button

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,23 +1,48 @@
-
-import { Component, EventEmitter, Output, Input } from '@angular/core';
-import { AuthUser } from '../auth/auth';
+import { Account } from './api/account.d';
+import { Component, EventEmitter, Output, Input, ChangeDetectionStrategy, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Input() public title: string;
-  @Input() public user: AuthUser;
+  @Input() public user: Account;
   @Input() public isUserLogged: boolean;
   @Output() public logoutHandler: EventEmitter<any> = new EventEmitter();
   @Output() public loginHandler: EventEmitter<any> = new EventEmitter();
+
+  public isMaingPage: boolean;
+
+  constructor(
+    private activeRouter: Router,
+    private location: Location,
+    private cdr: ChangeDetectorRef) { }
+
+  public ngOnInit() {
+    this.activeRouter.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/') {
+          this.isMaingPage = true;
+        } else {
+          this.isMaingPage = false;
+        }
+        this.cdr.detectChanges();
+      }
+    });
+  }
 
   public logout() {
     this.logoutHandler.emit();
   }
   public login() {
     this.loginHandler.emit();
+  }
+  public goBack() {
+    this.location.back();
   }
 }
