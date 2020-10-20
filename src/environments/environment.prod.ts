dcb9a417448a0e6a7098b3681e983509558cc203
jordi lopez galera <jordi8284@gmail.com>
2020-10-20 17:42:58 +0200

    revert dynamic hostiname in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,15 @@
 import * as jsonPackage from './../../package.json';
 const domain = 'jordi-lopez.eu.auth0.com';
-const hostname = window.location.hostname;
-const protocol = window.location.protocol;
+
 export const environment = {
   production: true,
   appName: 'La teva carta ',
-  version: jsonPackage.version,
+  version:  jsonPackage.version,
   auth: {
     clientID: 'WhSzGqyXoczFWt1GOXaTbV7kevavhfIh',
     domain,
-    redirectUri: `${protocol}://${hostname}/callback`,
-    returnTo: `${protocol}://${hostname}`,
+    redirectUri: 'https://admin.latevacarta.click/callback',
+    returnTo: 'https://admin.latevacarta.click',
     scope: 'openid profile email',
     audience: `https://${domain}/userinfo`,
     responseType: 'token id_token',
