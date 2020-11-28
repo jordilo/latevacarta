3997f50437ac4d87b0e700f44c69139d1f2d3ff9
jordi lopez galera <jordi8284@gmail.com>
2020-11-28 11:06:29 +0100

    print qr properly

--- src/app/views/business-qr/business-qr.component.ts
+++ src/app/views/business-qr/business-qr.component.ts
@@ -1,35 +1,78 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels, QrcodeComponent } from '@techiediaries/ngx-qrcode';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+import html2canvas from 'html2canvas';
+import jspdf from 'jspdf';
+import { combineLatest, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { IBusiness } from 'src/app/api/business';
+import { BusinessService } from '../../api/business.service';
 
 @Component({
   selector: 'app-business-qr',
   templateUrl: './business-qr.component.html',
-  styleUrls: ['./business-qr.component.css'],
+  styleUrls: ['./business-qr.component.scss'],
 })
 export class BusinessQrComponent implements OnInit {
 
   public elementType = NgxQrcodeElementTypes.URL;
   public correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
 
-  @ViewChild(QrcodeComponent, { static: false }) public qr: QrcodeComponent;
-  public url$: Observable<string>;
+  @ViewChildren('content') public contents: QueryList<ElementRef<HTMLDivElement>>;
+  public data$: Observable<{ business: IBusiness, url: string }>;
 
   public hostname = window.location.hostname.replace('admin.', '');
+  public form: FormGroup;
+  public sizes = [{
+    name: 'small',
+    size: 250,
+  },
+  {
+    name: 'medium',
+    size: 500,
+  },
+  {
 
-  constructor(private activatedRouter: ActivatedRoute) { }
+    name: 'large',
+    size: 1000,
+  }];
+
+  constructor(private business: BusinessService, private activatedRouter: ActivatedRoute, private fb: FormBuilder) { }
 
   public ngOnInit(): void {
+
+    this.form = this.fb.group({
+      size: this.sizes[0].size,
+    });
+
     // TODO fins the way to extract params from parent
-    this.url$ = this.activatedRouter.params.pipe(map(({ businessId }) => {
+    const url$ = this.activatedRouter.params.pipe(map(({ businessId }) => {
       const url = `${window.location.protocol}//qr.${this.hostname}/${businessId}`;
       return url;
     }));
+    const business$ = this.activatedRouter.params.pipe(switchMap(({ businessId }) => {
+      return this.business.getById(businessId);
+    }));
+    this.data$ = combineLatest([business$, url$]).pipe(map((data) => ({ business: data[0], url: data[1] })));
   }
   public print() {
+
+    const element = this.contents.find(({ nativeElement }) => nativeElement.id === 'print');
     // tslint:disable-next-line:no-console
-    console.log(this.qr.qrcElement.nativeElement.querySelector('img').src);
+    // console.log(this.qr.qrcElement.nativeElement.querySelector('img').src);
+    html2canvas(element.nativeElement).then((canvas) => {
+      const contentDataURL = canvas.toDataURL('image/png');
+      const width = this.pxToCm(canvas.width);
+      const height = this.pxToCm(canvas.height);
+      // context.drawImage(canvas, 0, 0);
+      const pdf = new jspdf('p', 'cm', [width, height]); // Generates PDF in landscape mode
+      // let pdf = new jspdf('p', 'cm', 'a4'); Generates PDF in portrait mode
+      pdf.addImage(contentDataURL, 'WEBP', 0, 0, width, height);
+      pdf.save('Filename.pdf');
+    });
+  }
+  private pxToCm(pixel: number): number {
+    return pixel / 96 * 2.54;
   }
 }
