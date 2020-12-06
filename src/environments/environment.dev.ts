b8725bed0ca910cd258f8aebc73472da3196e918
jordi lopez galera <jordi8284@gmail.com>
2020-12-06 10:53:55 +0100

    fix errors in dev environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,8 +5,8 @@ const domain = 'jordi-lopez.eu.auth0.com';
 export const environment = {
   production: true,
   appName: 'La teva carta ',
-  graphQlUrl: 'https://latevacarta-database.herokuapp.com/v1/graphql',
-  backendUrl: 'https://backend-ltc.herokuapp.com/',
+  graphQlUrl: 'https://latevacarta.herokuapp.com/v1/graphql',
+  backendUrl: 'https://backend-tlc.herokuapp.com/',
   googleMapsApi: 'AIzaSyBFWvZ2Aoes_dRFk36ZQz65ip0TDutUTk4',
   files: {
     product_template: 'https://latevacarta.s3.eu-central-1.amazonaws.com/assets/products.xlsx',
@@ -15,7 +15,7 @@ export const environment = {
   auth: {
     clientID: 'WhSzGqyXoczFWt1GOXaTbV7kevavhfIh',
     domain,
-    redirectUri: 'https://dev-dmin.latevacarta.click/callback',
+    redirectUri: 'https://dev-admin.latevacarta.click/callback',
     returnTo: 'https://dev-admin.latevacarta.click',
     scope: 'openid profile email',
     audience: `https://${domain}/userinfo`,
