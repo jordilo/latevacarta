4962381575ef378a4dbf17fa71ca61ff39a9b8a4
jordi lopez galera <jordi8284@gmail.com>
2020-12-19 15:44:08 +0100

    add some analytics in business details

--- src/app/views/business-detail/business-detail.component.ts
+++ src/app/views/business-detail/business-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { IBusiness } from 'src/app/api/business';
+import { IAnalytics } from '../../api/analytics';
+import { AnalyticsService } from '../../api/analytics.service';
 import { BusinessService } from './../../api/business.service';
 
 @Component({
@@ -13,11 +15,22 @@ import { BusinessService } from './../../api/business.service';
 export class BusinessDetailComponent implements OnInit {
 
   public business$: Observable<IBusiness>;
-  constructor(private router: ActivatedRoute, private businessService: BusinessService) { }
+  public firstVisit$: Observable<IAnalytics>;
+  public views$: Observable<IAnalytics>;
+  constructor(
+    private router: ActivatedRoute,
+    private businessService: BusinessService,
+    private analyticsService: AnalyticsService,
+  ) { }
 
   public ngOnInit(): void {
     this.business$ = this.router.params
       .pipe(switchMap(({ businessId }) => this.businessService.getById(businessId)));
+
+    this.firstVisit$ = this.router.params
+      .pipe(switchMap(({ businessId }) => this.analyticsService.getFirstVisit(businessId)));
+    this.views$ = this.router.params
+      .pipe(switchMap(({ businessId }) => this.analyticsService.getViews(businessId)));
   }
 
 }
