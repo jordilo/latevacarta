d3744124f838a2e4dade0f54e1498806cddd02d4
jordi lopez galera <jordi8284@gmail.com>
2020-12-19 19:21:23 +0100

    add charts to business

--- src/app/views/business-detail/business-detail.component.ts
+++ src/app/views/business-detail/business-detail.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartDataSets } from 'chart.js';
+import moment from 'moment';
+import { Label } from 'ng2-charts';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { IBusiness } from 'src/app/api/business';
 import { IAnalytics } from '../../api/analytics';
 import { AnalyticsService } from '../../api/analytics.service';
 import { BusinessService } from './../../api/business.service';
+
+interface IAnalyticsChartData {
+  data: ChartDataSets[];
+  dates: Label[];
+}
+
+function mapAnalytics(data: IAnalytics, totalDays): IAnalyticsChartData {
+  const firstDay = moment().add(-28, 'days');
+  const allData = [...Array(totalDays).keys()]
+    .map(() => {
+      let currentValue = 0;
+      const currentDay = firstDay.add(1, 'days').format('YYYY-MM-DD');
+      const index = data.dates.findIndex((date) => moment(date).format('YYYY-MM-DD') === currentDay);
+      if (index !== -1) {
+        currentValue = data.data[index];
+      }
+
+      return [currentValue, currentDay];
+
+    });
+  return {
+    data: [{ data: allData.map((d) => d[0]), label: 'views' }],
+    dates: allData.map((d) => d[1]),
+  } as IAnalyticsChartData;
+}
 
 @Component({
   selector: 'app-business-detail',
@@ -15,8 +43,8 @@ import { BusinessService } from './../../api/business.service';
 export class BusinessDetailComponent implements OnInit {
 
   public business$: Observable<IBusiness>;
-  public firstVisit$: Observable<IAnalytics>;
-  public views$: Observable<IAnalytics>;
+  public firstVisit$: Observable<IAnalyticsChartData>;
+  public views$: Observable<IAnalyticsChartData>;
   constructor(
     private router: ActivatedRoute,
     private businessService: BusinessService,
@@ -28,9 +56,12 @@ export class BusinessDetailComponent implements OnInit {
       .pipe(switchMap(({ businessId }) => this.businessService.getById(businessId)));
 
     this.firstVisit$ = this.router.params
-      .pipe(switchMap(({ businessId }) => this.analyticsService.getFirstVisit(businessId)));
+      .pipe(switchMap(({ businessId }) => this.analyticsService.getFirstVisit(businessId)),
+        map((data) => mapAnalytics(data, 28)));
     this.views$ = this.router.params
-      .pipe(switchMap(({ businessId }) => this.analyticsService.getViews(businessId)));
+      .pipe(
+        switchMap(({ businessId }) => this.analyticsService.getViews(businessId)),
+        map((data) => mapAnalytics(data, 28)));
   }
 
 }
